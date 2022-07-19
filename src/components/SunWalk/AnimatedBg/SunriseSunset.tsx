d1cb38c4c9eab55a44ec378470d98e3a133652af
Eugene <12qweR10y>
2022-07-19 23:44:01 +0300

    fix sunrise bg

--- src/components/SunWalk/AnimatedBg/SunriseSunset.tsx
+++ src/components/SunWalk/AnimatedBg/SunriseSunset.tsx
@@ -4,21 +4,32 @@ import { animated, useSpring } from 'react-spring';
 import s from './style.module.scss';
 
 interface SunriseSunsetBgProps {
-    nowValue: number;
+    value: number;
     max: number;
 }
 
-const SunriseSunsetBg: React.FC<SunriseSunsetBgProps> = ({ nowValue, max }) => {
+const SunriseSunsetBg: React.FC<SunriseSunsetBgProps> = ({ value, max }) => {
+    const nowGradientValue = Math.floor((value * 60) / max)
+
+    let opacityChanger = 0
+    if (value < max / 2) {
+        opacityChanger = Math.trunc((value / (max / 2)) * 100) / 100
+    } else {
+        opacityChanger = Math.trunc((1 - ((value - (max / 2)) / (max - (max / 2)))) * 100) / 100
+    }
+
+
+
     const animatedProps = useSpring({
         to: {
-            backgroundImage: 'linear-gradient(#3881ff ' + nowValue + '%, #ff3838 100%)',
-            opacity: Math.trunc((1 - nowValue / max) * 100) / 100,
+            // backgroundImage: 'linear-gradient(#3881ff ' + nowGradientValue + '%, #ff3838 100%)',
+            opacity: opacityChanger,
         },
         from: {
             backgroundImage: 'linear-gradient(#3881ff 0%, #ff3838 100%)',
-            opacity: 1,
+            opacity: 0,
         },
-        config: { mass: 1, tension: 2000, friction: 100 },
+        config: { mass: 1, tension: 2000, friction: 500 },
     });
 
     return <animated.div className={s.sunriseSunsetBg} style={animatedProps} />;
--- src/components/SunWalk/SunWalk.tsx
+++ src/components/SunWalk/SunWalk.tsx
@@ -12,11 +12,8 @@ export interface GeoProps {}
 
 const SunWalk: React.FC<GeoProps> = () => {
     const data = useDayForecast((state) => state.data);
-    const max = 180;
+    const max = 226;
     const sunWalkResult = data && sunMove(data.sys.sunrise, data.sys.sunset, data.timezone, max);
-
-
-
 
     if (data) {
         console.log(data)
@@ -24,26 +21,26 @@ const SunWalk: React.FC<GeoProps> = () => {
         console.log('Sunrise', dateWithTimeOffset(new Date(data.sys.sunrise * 1000), data.timezone))
         console.log('Sunset', dateWithTimeOffset(new Date(data.sys.sunset * 1000), data.timezone))
     }
-    const {devNumber, night} = useControls({
-        devNumber: {
-            value: 0,
-            min: 0,
-            max: max,
-            step: 5
-        },
-        night: false
-    })
-
+    // const {devNumber, night} = useControls({
+    //     devNumber: {
+    //         value: 0,
+    //         min: 0,
+    //         max: max,
+    //         step: 10
+    //     },
+    //     night: false
+    // })
+    // const sunStyle = {
+    //     transform: `translateY(-${devNumber}%)`,
+    // };
 
     const sunStyle = {
         transform: `translateY(-${sunWalkResult ? sunWalkResult.value : 1}%)`,
     };
 
-    const nowInRange = Math.floor(((sunWalkResult ? sunWalkResult.value : 1) * 60) / max);
-
     return (
         <div className={s.sunContainer}>
-            <SunriseSunsetBg nowValue={nowInRange} max={max} />
+            <SunriseSunsetBg value={sunWalkResult ? sunWalkResult.value : 1} max={max} />
             {/*<DayNightBg nowValue={devNumber} />*/}
 
             <div className={s.sun} style={sunStyle}>
